b1aa2fb2f64a7366906a5bd1bf4896f00f9cb745
TomRaaff <tom.raaff@jcore.com>
2021-08-08 12:35:26 +0200

    a product summary component

--- src/ts/webshop/components/ProductSummary.ts
+++ src/ts/webshop/components/ProductSummary.ts
@@ -1,17 +1,67 @@
-import { Component, div, h2 } from 'tr-utilities-lib';
+import { article, button, Component, div, h3, h4, i, img } from 'tr-utilities-lib';
 import { Product } from '../types/Product';
 
 export class ProductSummaryComponent extends Component {
 
 	constructor(product: Product) {
 		super();
-		this.setState({product});
+		this.setState({ product });
+	}
+
+	navToDetails() {
+		console.log("navToDetails", {state: this.state, loc: window.location});
+		window.location.href = 'details-page.html';
+	}
+
+	truncate(text: string): string {
+		return text.slice(0, 150).concat('...');
 	}
 
 	render(): HTMLElement {
-		return div(
-				{ class: 'product-summary-component' },
-				h2(`${this.state.product.productName}`),
+		const p = this.state.product;
+		return article(
+				{ onclick: this.navToDetails.bind(this) },
+				div(
+					{ class: 'img-container' },
+					img({ src: p.imageUrl })
+				),
+				div({ class: 'info-container' },
+					div(
+						{ class: 'product-title' },
+						h4(`${p.brandName} ${p.productName}`),
+						h3(`€${p.price}`)
+					),
+					div({class: 'rating'},
+						i({class: 'fas fa-star'}),
+						i({class: 'fas fa-star'}),
+						i({class: 'fas fa-star'}),
+						i({class: 'fas fa-star'}),
+						i({class: 'far fa-star'}),
+					),
+					div({class: 'description'},
+						this.truncate(p.description)
+					),
+					div({class: 'buttons'},
+						button({ class: 'primary' },
+							i({class: 'fas fa-shopping-basket'}),
+							' buy'
+						)
+					)
+				)
 		);
 	}
 }
+
+// <article>
+// 		<div class="img-container">
+// 			<img src="/img/gretsch.jpg" alt="a Gretsch drumkit">
+// 		</div>
+// 		<div class="info-container">
+
+// 			<div class="buttons">
+// 				<button class="primary">
+// 					<i class="fas fa-shopping-basket"/></i> Buy
+// 				</button>
+// 			</div>
+// 		</div>
+// </article>
--- src/ts/webshop/products-page.ts
+++ src/ts/webshop/products-page.ts
@@ -2,15 +2,23 @@ import { HeaderComponent } from '../components/HeaderComponent';
 import { Collection } from 'tr-utilities-lib';
 import { getDrumkits } from './backend/Drumkits';
 import { Product } from './types/Product';
+import { ProductSummaryComponent } from './components/ProductSummary';
 
 (async function () {
-	console.log("Products-page loaded");
+	console.log('Products-page loaded');
 	document.querySelector('body')!
 			.prepend(new HeaderComponent().render());
 
 	getDrumkits()
 			.then((products: Collection<Product>) => {
-
+				products.map((product) => new ProductSummaryComponent(product))
+						.forEach((productComponent) => {
+							document.getElementById('products-list')!
+									.append(productComponent.render());
+						});
+			})
+			.finally(() => {
+				document.querySelector('.spinner')!.classList.add('invisible');
 			});
 
-})()
+})();
