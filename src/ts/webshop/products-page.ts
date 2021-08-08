8dd89887ce640630bb0328900964467a85d945c4
TomRaaff <tom.raaff@jcore.com>
2021-08-08 11:40:10 +0200

    implemented a draft version for the products page

--- src/ts/webshop/products-page.ts
+++ src/ts/webshop/products-page.ts
@@ -1,7 +1,16 @@
 import { HeaderComponent } from '../components/HeaderComponent';
+import { Collection } from 'tr-utilities-lib';
+import { getDrumkits } from './backend/Drumkits';
+import { Product } from './types/Product';
 
-(function () {
+(async function () {
 	console.log("Products-page loaded");
 	document.querySelector('body')!
 			.prepend(new HeaderComponent().render());
+
+	getDrumkits()
+			.then((products: Collection<Product>) => {
+
+			});
+
 })()
